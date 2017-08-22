d113b05f3295697da4ae5bb79f98f7826cc232a9
HalilCan <halilcanmemoglu@gmail.com>
2017-08-22 02:08:36 -0400

    Rolled back decoding since the InventorSpacexyz team decided to use Go for server-side. I cannot help with that.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ let emailBox = document.getElementById('em');
 let email = '';
 let schoolBox = document.getElementById('sc');
 let school = '';
-let resultBox = document.getElementById('result');
 
 function getInput() {
   firstName = firstNameBox.value;
